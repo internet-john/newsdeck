ded446c53d40cf685c16f79f9278c9e3893b7517
internet-john <holman.jx@gmail.com>
2024-06-11 12:44:19 -0400

    bookmarks feature e2e

--- src/components/Article/Article.tsx
+++ src/components/Article/Article.tsx
@@ -1,15 +1,34 @@
 import React from "react";
 import { ArticleProps } from "../../types/Article";
 import "./Article.css";
+import useBookmarksStore from "../../stores/bookmarksStore";
 
-const Article: React.FC<ArticleProps> = ({ article, idx }) => (
-  <li className="articles__aside--listing" key={idx}>
-    <a target="_blank" rel="noreferrer" id="listing__link" href={article.url}>
-      <div id="listing__textwrapper">
-        <div id="listing__title">{article.title}</div>
-      </div>
-    </a>
-  </li>
-);
+const Article: React.FC<ArticleProps> = ({ article, idx }) => {
+  const { bookmarks, addBookmark, removeBookmark } = useBookmarksStore();
+
+  const isAlreadyBookmarked = bookmarks.find(
+    (bookmark) => bookmark.title === article.title
+  );
+
+  return (
+    <li className="articles__aside--listing" key={idx}>
+      <a target="_blank" rel="noreferrer" id="listing__link" href={article.url}>
+        <div id="listing__textwrapper">
+          <div id="listing__title">{article.title}</div>
+        </div>
+      </a>
+      <button
+        id="bookmark__button"
+        onClick={() =>
+          isAlreadyBookmarked
+            ? removeBookmark(article, bookmarks)
+            : addBookmark(article, bookmarks)
+        }
+      >
+        {isAlreadyBookmarked ? "Remove Bookmark" : "Add Bookmark"}
+      </button>
+    </li>
+  );
+};
 
 export default Article;
--- src/components/Bookmarks/Bookmark.tsx
+++
@@ -1,8 +0,0 @@
-interface BookmarkProps {
-  bookmarkTitle: string;
-}
-const Bookmark: React.FC<BookmarkProps> = ({ bookmarkTitle }) => {
-  return <div>{bookmarkTitle}</div>;
-};
-
-export default Bookmark;
